947affd38a93995de6df01437b838f5d57d61bb9
sophia <108816332+sophiaishii@users.noreply.github.com>
2023-08-20 23:14:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 
 from AF import AF
 from AFND import AFND
+from AFE import AFE
 
 if __name__ == "__main__":
     # path = "det/entrada.txt"
@@ -11,10 +12,19 @@ if __name__ == "__main__":
     # output = open("det/output.txt", "w")
     # maquina = AF(data["inicial"], data["final"], data["transitions"], path)
     # maquina.verificarCondicao(output)
+    
     path = "ndet/ex2_input.txt"
     with open("ndet/automato.json", "r") as f:
         data = json.load(f)
     #Cria o arquivo output
     output = open("ndet/output.txt", "w")
     maquina = AFND(data["inicial"], data["final"], data["transitions"], path)
+    maquina.verificarCondicao(output)
+
+    # path = "mvazio/entrada.txt"
+    # with open("mvazio/automato.json", "r") as f:
+    #    data = json.load(f)
+    # cria o arquivo output
+    # output = open("mvazio/output.txt", "w")
+    # maquina = AFE(data["inicial"], data["final"], data["transitions"], path)
     maquina.verificarCondicao(output)
