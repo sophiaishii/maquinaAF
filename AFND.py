eccdc5feaad05bb2ad19a5edab2459f61cda11d0
Sophia Ishii <sophiaishiidognani@hotmail.com>
2023-08-20 21:31:36 -0300

    AFND

--- AFND.py
+++ AFND.py
@@ -2,7 +2,7 @@ import time
 import timeit
 
 
-class AF:
+class AFND:
     def __init__(self, inicial, final, transitions, path): #construtor eh o q instancia a classe e inicializa os atributos
         self.inicial = inicial #self eh o proprio objeto(atributo)
         self.final = final
@@ -21,29 +21,47 @@ class AF:
     def verificarCondicao(self, output):
         #começo do tempo de execução
         tempo_inicial = timeit.default_timer()
-
         for linha in self.lista_input:
             #inicializar o estado atual com o estado inicial
-            estado_atual = self.inicial
+            estado_atual = []
+            estado_atual.append(self.inicial)
             #para cada caracter da linha
             for caracter in linha:
                 if caracter == ";":
                     break
-                for j in self.transitions:
-                    if j["from"] == estado_atual:
-                        if j["read"] == caracter:
-                            estado_atual = j["to"]                   
-                            break
-                else:
-                    estado_atual = "erro"
+                estado_atual_aux = []
+                for estado in estado_atual:
+                    for j in self.transitions:
+                        if j["from"] == estado:
+                            if j["read"] == caracter:
+                                if j["to"] not in estado_atual_aux:
+                                    estado_atual_aux.append(j["to"])
+                                    print(estado_atual_aux, caracter)
+                estado_atual = estado_atual_aux
+                if estado_atual == []:
+                    estado_atual = ["erro"]
                     break
-            if estado_atual in self.final:
+                
+    
+                    
+            print(estado_atual)
+            for estado in estado_atual:
+                if estado in self.final:
                 #Tempo final de execução
-                tempo_final = timeit.default_timer()-tempo_inicial
-                output.write(linha + ";1;"+str(round(tempo_final,5))+"\n")
+                    tempo_final = timeit.default_timer()-tempo_inicial
+                    output.write(linha + ";1;"+str(round(tempo_final,5))+"\n")
+                    break
             else:
                 #Tempo final de execução
                 tempo_final = timeit.default_timer()-tempo_inicial
                 output.write(linha + ";0;"+str(round(tempo_final,5))+"\n")
         output.close()
-
+    
+    def transicao(self, estado_atual, caracter):
+        for j in self.transitions:
+            if j["from"] == estado_atual:
+                if j["read"] == caracter:
+                    return j["to"]
+        return "erro"
+    
+        
--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import json
 
 from AF import AF
+from AFND import AFND
 
 if __name__ == "__main__":
-    path = "entrada.txt"
-    with open("automato.json", "r") as f:
+    # path = "det/entrada.txt"
+    # with open("det/automato.json", "r") as f:
+    #     data = json.load(f)
+    # #Cria o arquivo output
+    # output = open("det/output.txt", "w")
+    # maquina = AF(data["inicial"], data["final"], data["transitions"], path)
+    # maquina.verificarCondicao(output)
+    path = "ndet/ex2_input.txt"
+    with open("ndet/automato.json", "r") as f:
         data = json.load(f)
     #Cria o arquivo output
-    output = open("output.txt", "w")
-    maquina = AF(data["inicial"], data["final"], data["transitions"], path)
+    output = open("ndet/output.txt", "w")
+    maquina = AFND(data["inicial"], data["final"], data["transitions"], path)
     maquina.verificarCondicao(output)
